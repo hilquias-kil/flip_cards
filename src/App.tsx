36372b6dea5899b9863132bc736989134e3adaa3
Hilquias O. Rodrigues <hilquias.kil@gmail.com>
2022-10-05 22:09:34 -0300

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
